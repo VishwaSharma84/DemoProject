79f46e45d27fc3810a39493a041d4be9c706a0e5
VishwaSharma84 <127947793+VishwaSharma84@users.noreply.github.com>
2024-02-05 12:10:31 +0530

    Updated data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def extract_data():
-    data = pd.read_csv("https://raw.githubusercontent.com/JyothsnaPendyala/Customer-Segmentation/main/segmentation%20data.csv")
+    data = pd.read_csv("segmentation data.csv")
     print(data.head())
     return data
 
